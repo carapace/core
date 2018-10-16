d986354643c3d375194b3db86a4614e8b15c7c89
AbeIgnocious <k.l.kubat@gmail.com>
2018-10-16 16:15:13 +0200

    add Strategy api to globally set test level

--- pkg/suite/suite.go
+++ pkg/suite/suite.go
@@ -2,7 +2,9 @@ package test
 
 import (
 	"github.com/ory/dockertest"
+	"github.com/spf13/viper"
 	"go.uber.org/zap"
+	"testing"
 )
 
 var pool *dockertest.Pool
@@ -16,4 +18,33 @@ func init() {
 		Logger.Panic("Could not connect to docker", zap.Error(err))
 	}
 	Logger.Info("finished pool initialization")
+}
+
+const (
+
+	// A unit test requires no outside services
+	Unit = 0
+
+	// LocalIntegration tests require DB's and other services, which can be deployed locally
+	LocalIntegration = 1
+
+	// Integration tests require services outside of our control
+	Integration = 2
+
+	// E2E tests are long, expensive end to end tests
+	E2E = 3
+)
+
+// Setting up viper to parse flags and environment variables
+func init() {
+	viper.BindEnv("TEST_LEVEL")
+	viper.SetDefault("TEST_LEVEL", 1)
+}
+
+// Strategy parses environment variables and flags, and depending on global presets,
+// skips tests.
+func Strategy(t *testing.T, level int) {
+	if viper.GetInt("TEST_LEVEL") < level {
+		t.Skip("skipping test, level too high")
+	}
 }
