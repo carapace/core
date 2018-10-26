15d88b5ca42cfd74227adb7f0e7d085080273b02
AbeIgnocious <k.l.kubat@gmail.com>
2018-10-26 14:03:37 +0200

    improve doc, incorporate chain description

--- pkg/chain-db/doc.go
+++ pkg/chain-db/doc.go
@@ -1,8 +1,9 @@
 /*
 package chain-db defines an append only K,V store, permanently storing all versions of the object
 
-An object in chain-db starts out by defining all possible fields through protocol buffers, and then only being
-able to amend the object. Gets always return a Chain object, which contains every single put on the object, verifyable by
-the hashes
+An object in chain-db is a proto.Message, marshalled into an any.Any and embedded with metadata on state and key.
+
+When querying, a chain of all puts on that object is returned, and the chain is verified for integrity. It is
+up to the caller to unmarshal their values into appropriate structs.
 */
 package append
