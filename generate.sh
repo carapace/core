3ec3eacd347c26df714c95581702a29c3c9a94ce
AbeIgnocious <k.l.kubat@gmail.com>
2018-11-21 17:36:24 +0100

    rename proto.sh to .proto.sh
    this way it is nicely sorted
    
    Signed-off-by: AbeIgnocious <k.l.kubat@gmail.com>

--- generate.sh
+++ generate.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-COMMAND='./proto.sh'
-find `pwd` -iname "proto.sh" -printf "%h\n" | sort -u | while read i; do
+COMMAND='./.proto.sh'
+find `pwd` -iname ".proto.sh" -printf "%h\n" | sort -u | while read i; do
     cd "$i" && pwd && $COMMAND
 done
